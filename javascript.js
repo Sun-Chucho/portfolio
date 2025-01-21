e230f6c5c76f1e6df1c5a2295f654f06bbcec03f
Sun-Chucho <ogollachucho@gmail.com>
2025-01-21 02:47:55 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+
+  // Toggle the 'active' class when the hamburger menu is clicked
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+  });
+  
